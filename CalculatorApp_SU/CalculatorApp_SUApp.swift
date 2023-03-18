38c7a5a3e2e628a9e4b39a0dee6aaf1b581655d1
Nadia <ivashchenko.nadia@gmail.com>
2023-03-18 22:07:34 +0200

    Initial Commit

---
+++ CalculatorApp_SU/CalculatorApp_SUApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp_SUApp.swift
+//  CalculatorApp_SU
+//
+//  Created by Students on 18.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp_SUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
