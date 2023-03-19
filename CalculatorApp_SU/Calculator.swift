766b7f447ed9b4c0de63bdd1dc1e2a01370acc86
Nadia <ivashchenko.nadia@gmail.com>
2023-03-19 23:15:21 +0200

    Set Digit

--- CalculatorApp_SU/Calculator.swift
+++ CalculatorApp_SU/Calculator.swift
@@ -9,8 +9,43 @@ import SwiftUI
 
 struct Calculator {
     
+    //MARK: Properties
+    
     var text: String {
         return "0"
+    }
+    
+    // Empty Decimal: creates a decimal initialized to 0.
+    private var newNumber: Decimal?
+    
+    private var currentNumber: Decimal? {
+        newNumber
+    }
+    
+    var displayText: String {
+        return getNumberString(number: currentNumber, withCommas: true)
+    }
+    
+    
+    
+    //MARK: Functions
+    
+    private func getNumberString(number: Decimal?, withCommas: Bool = false) -> String {
+        return (withCommas ? number?.formatted(.number) : number.map(String.init)) ?? "0" // ?
+    }
+    
+    // check if calculator can add digit: number not nil or digit not 0
+    private func checkIsAdd(_ digit: Digit) -> Bool {
+        return currentNumber != nil || digit != .zero
+    }
+    
+    mutating func setDigit(_ digit: Digit) {
+        // check if you can add currentNumber to numberString: You can not do this if currentNumber = nil or if digit = 0 (0 replace to next digit)
+        guard checkIsAdd(digit) else { return }
+        // get newNumber and convert it to string
+        let numberString = getNumberString(number: newNumber)
+        // add digit to the end of numberString and convert it to string of type Decimal and assign it to newNumber
+        newNumber = Decimal(string: numberString.appending("\(digit.rawValue)"))
     }
     
     mutating func allClear() {
@@ -27,10 +62,7 @@ struct Calculator {
     
     mutating func setOperation(_ operation: ArithmeticOperation) {
     }
-    
-    mutating func setDigit(_ digit: Digit) {
-    }
-    
+        
     mutating func setDecimal() {
     }
     
